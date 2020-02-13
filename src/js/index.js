8475bd349ee0e2d2bb3b2f534f90ecf2ee132a8f
Hailey Bae <hwisunbae.hb@gmail.com>
2020-02-13 16:06:15 +0900

    Search.js in process

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,34 @@
-import x from './test';
-const num = 23;
-console.log(`imported ${x} from another module and num is ${num}`);
+import Search from './models/Search';
+
+/** Global state of the app
+ *  - search object
+ *  - current recipe object
+ *  - shopping list object
+ *  - liked recipes
+ */
+const state = {};
+
+/**
+ * 1. get query from view
+ * 2. new search object and add to state
+ * 3. prepare UI for results
+ * 4. search for recipes
+ * 5. render results on UI
+ */
+const controlSearch = async () => {
+    const query = 'pizza';
+
+    if (query) {
+        state.search = new Search(query);
+
+        await state.search.getResults();
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.search').addEventListener('submit' ,e => {
+    e.preventDefault();
+    controlSearch();
+
+});
+
