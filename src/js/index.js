b160c0ad8b47a5e353dc1784b928c2e83a8bcfce
Hailey Bae <hwisunbae.hb@gmail.com>
2020-02-14 16:37:45 +0900

    in progress to build recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
-import { elements} from "./views/base";
+import Recipe from './models/Recipe';
+import {clearLoader, elements, renderLoader} from "./views/base";
 import * as searchView from './views/searchView';
 
 
@@ -10,6 +11,10 @@ import * as searchView from './views/searchView';
  *  - liked recipes
  */
 const state = {};
+
+/* ----------------------
+       SEARCH CONTROL
+-------------------------*/
 
 /**
  * 1. get query from view
@@ -25,15 +30,71 @@ const controlSearch = async () => {
 
     if (query) {
         state.search = new Search(query);
+        searchView.clearInput();
+        searchView.clearResult();
 
-        await state.search.getResults();
-        console.log(state.search.result);
+        renderLoader(elements.searchRes);
+
+        try{
+            await state.search.getResults();
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something went with the search ...')
+            clearLoader();
+        }
+
+
+
     }
 };
 
 elements.searchForm.addEventListener('submit' ,e => {
     e.preventDefault();
     controlSearch();
-
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResult();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+
+/* ----------------------
+       RECIPE CONTROL
+-------------------------*/
+
+/**
+ * Get ID from URL
+ * UI for changes
+ * create new recipe object
+ * get recipe data
+ * calculate servings and time
+ * render Recipe
+ */
+const controlRecipe = async () => {
+    const id = window.location.hash.replace('#', '');
+
+    if (id) {
+        state.recipe = new Recipe(id);
+
+        try{
+            await state.recipe.getRecipe();
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            console.log(state.recipe);
+        } catch (e) {
+            //TODO: HTML can be implemented
+            alert('Error processing Recipe');
+        }
+    }
+}
+
+// window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('load', controlRecipe);
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,22 +2,88 @@ import { elements} from "./base";
 
 export const getInput = () => elements.searchInput.value;
 
-const renderRecipe = recipe => {
-const markup = `
-    <li>
-        <a class="results__link" href="#${recipe.recipe_id}">
-            <figure class="results__fig">
-                <img src="${recipe.image_url}" alt="Test">
-            </figure>
-            <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
-                <p class="results__author">${recipe.publisher}</p>
-            </div>
-        </a>
-    </li>
-`
+export const clearInput = () => {
+    elements.searchInput.value = '';
 };
 
-export const renderResults = recipes => {
-    recipes.forEach(el => renderRecipe(el));
+export const clearResult = () => {
+    elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 };
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    const start = (page-1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(el => renderRecipe(el));
+    renderButtons(page, recipes.length, resPerPage);
+};
+
+
+/**
+ * @param type 'prev' | 'next'
+ */
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev'? page-1: page +1}>
+        <span>Page ${type === 'prev'? page - 1: page +1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev'? 'left': 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+    let button;
+
+    if (page === 1 && pages > 1) {
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}        
+        `;
+    } else if (page === pages && pages > 1) {
+        button = createButton(page, 'prev');
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
+
+/**
+ * Cauliflower Pizza Crust (with BBQ Chicken Pizza)
+ * acc : 0 / acc + cur = 10 / newTitle ='Cauliflower'
+ */
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit)
+                newTitle.push(cur);
+            return acc + cur.length;
+        }, 0);
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+};
+
+const renderRecipe = recipe => {
+    const markup = `
+        <li>
+            <a class="results__link" href="#${recipe.recipe_id}">
+                <figure class="results__fig">
+                    <img src="${recipe.image_url}" alt="Test">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
+                </div>
+            </a>
+        </li>
+    `;
+
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+};
+
